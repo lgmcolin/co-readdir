fbf14624174b6fa6a46dee23b4d4d5a4e555bbca
lgmrain <lgmcolin@gmail.com>
2015-03-25 22:31:18 +0800

    up

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var fs = require('co-fs');
 var each = require('co-each');
 var join = require('path').join;
-var coFilter = require('co-filter');
-var co = require('co');
 
-function* readDir(dir, ignore, filter) {
-  var filterFn = typeof filter === 'undefined' ? noDotFiles : filter; 
+function* readDir(dir, filter) {
+  var filterFn = typeof filter === 'undefined' ? noDotFiles : filter;
   var out = [];
 
   function* walk(dir) {
@@ -13,7 +11,7 @@ function* readDir(dir, ignore, filter) {
 
     yield each(nodes.filter(filterFn), function* (node) {
       var path = join(dir, node);
-      
+
       var stats = yield fs.lstat(path);
 
       if (stats.isDirectory()) {
